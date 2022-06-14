41d8fc51217086be2c14d71ca0484000f1b8210c
Austin <austin@CODINGBOOK-II.unifi.biogimp.com>
2022-06-13 20:50:09 -0700

    setting up structure and initial schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+    Query: {
+    thoughts: async (parent, { username }) => {
+        const params = username ? { username } : {};
+        return Thought.find(params).sort({ createdAt: -1 });
+    },
+},
+
+module.exports = resolvers;
