d83f967d322a2503d0d3781e2974477eb7bfc0bb
KingAusti <austin@thehenrys.xyz>
2022-06-13 21:00:28 -0700

    adding to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,23 @@ const resolvers = {
         const params = username ? { username } : {};
         return Thought.find(params).sort({ createdAt: -1 });
     },
+    thought: async (parent, { _id }) => {
+        return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+        return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+        return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
 },
 
 module.exports = resolvers;
